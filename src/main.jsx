ee76671d98b51859960895073ad7770bb781aa8f
NusratTabassum3 <tabassumnusrat3@gmail.com>
2025-06-29 12:38:50 +0600

    frontend layout done

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,19 @@ import Layout from "./layout/index.jsx";
 import About from "./pages/About.jsx";
 import Books from "./pages/Books.jsx";
 import Contact from "./pages/Contact.jsx";
+import FrontEndLayout from "./layout/FrontEndLayout.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<FrontEndLayout />}>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/books" element={<Books />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
+        </Route>
+
+        <Route path="/admin" element={<Layout />}>
+          <Route path="about" element={<About />}></Route>
+          <Route path="books" element={<Books />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
